1f08a24b5fc011a19ef180a0d9e5558a809b470c
MatejBaca <117350089+MatejBaca@users.noreply.github.com>
2025-01-19 15:17:37 +0100

    Update tab3.page.ts

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-tab3',
@@ -6,8 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss'],
   standalone: false,
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  paletteToggle = false;
+  backupFrequency = 'daily';
 
-  constructor() {}
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+    this.initializeDarkPalette(prefersDark.matches);
+    prefersDark.addEventListener('change', (mediaQuery) => this.initializeDarkPalette(mediaQuery.matches));
+  }
+
+  initializeDarkPalette(isDark: boolean) {
+    this.paletteToggle = isDark;
+    this.toggleDarkPalette(isDark);
+  }
+
+  toggleChange(event: CustomEvent) {
+    this.toggleDarkPalette(event.detail.checked);
+  }
+
+  toggleDarkPalette(shouldAdd: boolean) {
+    document.documentElement.classList.toggle('ion-palette-dark', shouldAdd);
+  }
+
+  onBackupFrequencyChange(event: CustomEvent) {
+    console.log('Backup frequency set to:', event.detail.value);
+  }
 
 }
